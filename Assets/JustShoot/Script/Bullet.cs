56a495844214772e688ee7f81cc55414299f069d
Siko <bsj2234@naver.com>
2024-05-03 20:24:31 +0900

    총알이 맞은녀석의 인터페이스를 가지고 있을경우 그것을 통해 TakeDamage 호출

--- Assets/JustShoot/Script/Bullet.cs
+++ Assets/JustShoot/Script/Bullet.cs
@@ -14,12 +14,17 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         int type = 0;
-        if(collision.gameObject.CompareTag("Enemy"))//착탄한 대상의 태그가 Enemy일 경우, 이펙트를 피튀기는 이펙트로 변경
+
+        if (collision.gameObject.CompareTag("Enemy"))//착탄한 대상의 태그가 Enemy일 경우, 이펙트를 피튀기는 이펙트로 변경
         {
             type = 1;
+            collision.gameObject.GetComponent<IDamagable>().TakeDamage(dmg);
         }
-
-        //여기에 적에 데미지를 입히는 기능을 추가할 것.
+        else if (collision.gameObject.CompareTag("Player"))//착탄한 대상의 태그가 Player일 경우, 이펙트를 피튀기는 이펙트로 변경
+        {
+            type = 1;
+            collision.gameObject.GetComponent<IDamagable>().TakeDamage(dmg);
+        }
         ProjectileDestroy(collision.contacts[0].point, type);//충돌 위치를 넘겨서 각종 처리를 하는 역할.
     }    
     void ProjectileDestroy(Vector3 hitPosition, int type)
