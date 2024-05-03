5400b53ead8c895b3e762b40888e7d4d8eab5064
KDH <kidh1238@gmail.com>
2024-05-03 17:32:56 +0900

    20240503_01 UI는 프레임만 작업했음. 풀링은 진행 중 필요한 작업이 있어서 백업한 것.

---
+++ Assets/JustShoot/UI/EnemySpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public ObjectPoolManager poolManager;
+    public Queue<GameObject> m_Queue;
+
+    public GameObject[] prefabs;
+    public float spawnTime = 10.0f;
+    public int m_Count = 10;
+
+    private void Start()
+    {
+        //처음 생성할 때
+        //poolManager.CreatePool(prefabs[0], m_Count);
+
+        for (int i = 0; i < m_Count; i++)
+        {
+            //int e_rand = Random.Range(0, prefabs.Length);
+            GameObject m_object = poolManager.DequeueObject(prefabs[0]);
+            m_object.transform.position = transform.position;
+            m_object.gameObject.SetActive(false);
+
+            m_Queue.Enqueue(m_object);
+        }
+        StartCoroutine(EnemySpawn());
+    }
+
+    private IEnumerator EnemySpawn()
+    {
+        while (m_Queue.Count > 0)
+        {
+            GameObject Enemy = m_Queue.Dequeue();
+            Enemy.SetActive(true);
+
+            yield return new WaitForSeconds(spawnTime);
+            //m_Queue.Enqueue(prefabs[0]);
+        }
+
+    }
+}
