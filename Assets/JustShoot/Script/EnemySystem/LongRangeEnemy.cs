eaf7d83e75de83bcfb77e03739340c7e6de9bcf8
Siko <bsj2234@naver.com>
2024-05-03 16:09:29 +0900

    First Comit Enemies
    #made dealDamage Function and ShootProjectile

--- Assets/JustShoot/Script/EnemySystem/LongRangeEnemy.cs
+++ Assets/JustShoot/Script/EnemySystem/LongRangeEnemy.cs
@@ -1,15 +1,18 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class LongRangeEnemy : MonoBehaviour
 {
     [SerializeField] protected Player player;
     public enum State
     {
-        IDLE, TRACE, ATTACK, DIE
+        IDLE, TRACE, Aim, ATTACK, DIE
     }
     public State state = State.IDLE;
+
+    public bool isAimed = false;
 
     public float traceDistance = 10;
     public float attackDistance = 2;
@@ -17,13 +20,14 @@ public class Enemy : MonoBehaviour
     public bool isDie = false;
 
     Transform enemyTrf;
-    [SerializeField]Transform playerTrf;
+    [SerializeField] Transform playerTrf;
     NavMeshAgent agent;
     Animator animator;
     Statemachine statemachine;
 
     readonly int hashTrace = Animator.StringToHash("IsTrace");
     readonly int hashAttack = Animator.StringToHash("IsAttack");
+    readonly int hashAim = Animator.StringToHash("IsAim");
 
     public float hp = 100f;
 
@@ -53,19 +57,22 @@ public class Enemy : MonoBehaviour
         {
             yield return new WaitForSeconds(0.3f);
 
-            if(state == State.DIE)
+            if (state == State.DIE)
             {
                 statemachine.ChangeState(State.DIE);
                 yield break;
             }
 
             float distance = Vector3.Distance(playerTrf.position, enemyTrf.position);
-
-            if(distance <= attackDistance)
+            if(isAimed)
             {
-                statemachine.ChangeState(State.ATTACK);
+
             }
-            else if(distance <= traceDistance)
+            else if (distance <= attackDistance)
+            {
+                statemachine.ChangeState(State.Aim);
+            }
+            else if (distance <= traceDistance)
             {
                 statemachine.ChangeState(State.TRACE);
             }
@@ -76,11 +83,37 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void IsAimedPlayer()
+    {
+        //이거 재사용될 수 있음 fire 할때도 체크해야됨
+        Vector3 targetDir = (-transform.position + playerTrf.position).normalized;
+        transform.LookAt(targetDir);
+        bool isAimed = Vector3.Dot(targetDir, transform.forward) >= .99f;
+        if (isAimed)
+        {
+            isAimed = true;
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (isDie)
+        {
+            return;
+        }
+        hp-=damage;
+        if (hp <= 0f)
+        {
+            isDie = true;
+            //OnDead
+        }
+    }
+
 
     class BaseEnemyState : BaseState
     {
-        protected Enemy owner;
-        public BaseEnemyState(Enemy owner)
+        protected LongRangeEnemy owner;
+        public BaseEnemyState(LongRangeEnemy owner)
         {
             this.owner = owner;
         }
@@ -88,9 +121,9 @@ public class Enemy : MonoBehaviour
 
     class IdleState : BaseEnemyState
     {
-        public IdleState(Enemy owner) : base(owner) { }
+        public IdleState(LongRangeEnemy owner) : base(owner) { }
 
-        public override void Enter() 
+        public override void Enter()
         {
             owner.agent.isStopped = true;
             owner.animator.SetBool(owner.hashTrace, false);
@@ -99,7 +132,7 @@ public class Enemy : MonoBehaviour
 
     class TraceState : BaseEnemyState
     {
-        public TraceState(Enemy owner) : base(owner) { }
+        public TraceState(LongRangeEnemy owner) : base(owner) { }
 
         public override void Enter()
         {
@@ -113,12 +146,49 @@ public class Enemy : MonoBehaviour
 
     class AttackState : BaseEnemyState
     {
-        public AttackState(Enemy owner) : base(owner) { }
+        public AttackState(LongRangeEnemy owner) : base(owner) { }
 
         public override void Enter()
-        {                        
+        {
             owner.animator.SetBool(owner.hashAttack, true);
             Debug.Log("Shoot");
+        }
+        public override void Update()
+        {
+            //플레이어 조준 완료시 state Aimed 조건을 설정함
+            owner.IsAimedPlayer();
+        }
+    }
+    class AimState : BaseEnemyState
+    {
+        public AimState(LongRangeEnemy owner) : base(owner) { }
+
+        public override void Enter()
+        {
+            owner.animator.SetBool(owner.hashAim, true);
+            Debug.Log("Aim");
+        }
+
+        public override void Update()
+        {
+            //플레이어 조준 완료시 state Aimed 조건을 설정함
+            owner.IsAimedPlayer();
+        }
+    }
+    class DeadState : BaseEnemyState
+    {
+        public DeadState(LongRangeEnemy owner) : base(owner) { }
+
+        public override void Enter()
+        {
+            owner.animator.SetBool(owner.hashAim, true);
+            Debug.Log("Aim");
+        }
+
+        public override void Update()
+        {
+            //플레이어 조준 완료시 state Aimed 조건을 설정함
+            owner.IsAimedPlayer();
         }
     }
 }
