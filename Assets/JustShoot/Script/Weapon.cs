a33be2b1bb8987673393eb4b036ea0ae9dcd9dcd
CH.lee <114718113+elondogemoon@users.noreply.github.com>
2024-05-03 15:24:01 +0900

    (무기 프리팹 변경)Weapon클래스의 firePoint, fpsViewPoint 지정 및 조준경 장착

--- Assets/JustShoot/Script/Weapon.cs
+++ Assets/JustShoot/Script/Weapon.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Transform firePoint;//투사체 생성 위치
+    [SerializeField] Transform fpsViewPoint;//1인칭으로 볼 가상카메라 위치
+
     // Start is called before the first frame update
     void Start()
     {
